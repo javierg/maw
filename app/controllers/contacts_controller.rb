dc47b5366da6cce3d40ff367d76bb25ddd798979
javierg <javierg@amcoonline.net>
2013-10-12 07:15:52 -0700

    rescue from net error on smtp

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,11 +3,15 @@ class ContactsController < ApplicationController
   def new
     data = params[:contact]
     msg = ContactForm.new( data )
+  begin
     if msg.deliver
       flash[:info] = 'Gracias por tu interés. Pronto nos comunicaremos contigo para atender tus comentarios.'
     else
       flash[:error] = 'Ups! aparentemente algo salió mal. Por favor intenta de nuevo.'
     end
+  rescue Net::SMTPFatalError
+    flash[:error] = "Ups! ocurrió un error."
+  end
     redirect_to '/contacto'
   end
 
